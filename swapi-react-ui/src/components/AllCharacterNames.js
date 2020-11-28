3603ebf38c09aff8845425b29778eb394d030e8a
Austin Stowe <akstowe.95@gmail.com>
2020-11-27 18:43:26 -0800

    Started laying out AllCharacterNames component

--- swapi-react-ui/src/components/AllCharacterNames.js
+++ swapi-react-ui/src/components/AllCharacterNames.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
-export function AllCharacterNames() {
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import TextField from '@material-ui/core/TextField'
 
+
+const options = ['Option 1', 'Option 2'];
+
+export default function AllCharacterNames() {
+    return(
+        <>
+            <Autocomplete 
+                options = {options}
+                renderInput={(params) => <TextField {...params} variant="outlined" style={{width: '250px', padding:'10px'}}/>}
+            />
+        </>
+    )
     
 }
