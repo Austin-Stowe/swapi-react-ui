15fd07a4abfd9d92b05d2f4566ed8c783f5cca18
Austin Stowe <austinstowe@pop-os.localdomain>
2021-01-04 22:34:36 -0800

    Added recursive fetch

--- swapi-react-ui/src/components/AllCharacterNames.js
+++ swapi-react-ui/src/components/AllCharacterNames.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import TextField from '@material-ui/core/TextField'
 
 
-const options = ['Option 1', 'Option 2'];
+const options = [{name: 'option 1'},{name: 'option 2'}];
 
 export default function AllCharacterNames() {
+    const [characterArray, setCharacterArray] = useState([]);
+    const URL = 'https://swapi.dev/api/people/';
+
+    const fetchData = (apiUrl) => {
+        fetch(apiUrl)
+        .then(res=>res.json())
+        .then(data => {
+            apiUrl = data['next'];
+            setCharacterArray(characterArray + data['results'])
+            // Check next API url is empty or not, if not empty call the above function 
+            if(apiUrl !== '' && apiUrl !== null){
+                fetchData(apiUrl);
+            }
+        })
+    }   
+
+    useEffect(()=>{ 
+        fetchData(URL)
+    }, []);
+
+    useEffect(()=>{console.log(characterArray)},[characterArray])
+
     return(
         <>
-            <Autocomplete 
-                options = {options}
+            {options.map(idx => <div>{idx.name}</div>)}
+            {/* <Autocomplete 
+                options = {characterArray.map(idx => idx.name)}
                 renderInput={(params) => <TextField {...params} variant="outlined" style={{width: '250px', padding:'10px'}}/>}
-            />
+            /> */}
         </>
     )
     
