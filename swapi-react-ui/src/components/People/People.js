b0c410f89b1cdcae9de6c843c9c7fb1e7678f1fe
Austin Stowe <akstowe.95@gmail.com>
2021-01-06 17:34:46 -0800

    Got people data pulled and working

--- swapi-react-ui/src/components/People/People.js
+++ swapi-react-ui/src/components/People/People.js
@@ -1,50 +1,44 @@
 import React, { useState } from 'react';
-import { usePaginatedQuery } from 'react-query';
+import { useQuery } from 'react-query';
 
-const fetchPeople = async (key, page) => {
-  const res = await fetch(`http://swapi.dev/api/people/?page=${page}`);
-  return res.json();
-}
 
-const Planets = () => {
-  const [ page, setPage ] = useState(1);
-    {resolvedData, 
-    latestData, 
-    status 
-  } = usePaginatedQuery(['planets', page], fetchPeople);
+const People = (props) => {
+    const [page, setPage] = useState(1);
+    const { data, status } = useQuery(['people' ,page], 
+    () => fetch(`http://swapi.dev/api/people/?page=${page}`).then(res => res.json()), 
+    {
+        keepPreviousData: true
+    })
 
-  return (
-    <div>
-      <h2>People</h2>
-
-      {status === 'loading' && (
-        <div>Loading data</div>
-      )}
-
-      {status === 'error' && (
-        <div>Error fetching data</div>
-      )}
-
-      {status === 'success' && (
+    return (
         <>
-          <button 
-            onClick={() => setPage(old => Math.max(old - 1, 1))} 
-            disabled={page === 1}>
-            Previous Page
-          </button>
-          <span>{ page }</span>
-          <button 
-            onClick={() => setPage(old => (!latestData || !latestData.next ? old : old + 1))} 
-            disabled={!latestData || !latestData.next}>
-            Next page
-          </button>
-          <div>
-              {resolvedData.count}
-          </div>
+            <h2>People</h2>
+
+            {status === 'loading' && (
+                <div>Loading data</div>
+            )}
+
+            {status === 'error' && (
+                <div>Error fetching data</div>
+            )}
+
+            {status === 'success' && (
+                <>
+                    <button
+                        onClick={() => setPage(old => Math.max(old - 1, 1))}
+                        disabled={page === 1}>
+                        Previous Page
+                    </button>
+                    <button onClick={() => console.log(data)}>PRINT</button>
+                    <button
+                        onClick={() => setPage(old => (!data.next ? old : old + 1))}
+                        disabled={!data.next }>
+                        Next page
+                    </button>
+                </>
+            )}
         </>
-      )} 
-    </div>
-  );
+    );
 }
- 
-export default Planets;
+
+export default People;
