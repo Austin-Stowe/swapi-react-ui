783a5b366e12e16fe10a6bc7457cf3513f96002c
Austin Stowe <akstowe.95@gmail.com>
2021-01-06 17:36:54 -0800

    Updating App.js

--- swapi-react-ui/src/App.js
+++ swapi-react-ui/src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from "react-query/devtools";
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Container from '@material-ui/core/Container'
 import People from './components/People/People'
 
+const queryClient = new QueryClient()
 
-function App() {
+const App = () => {
   return (
-    <Container maxWidth="xl">
-      <People />
-    </Container>
+    <QueryClientProvider client={queryClient}>
+      <Container maxWidth="xl">
+        <People />
+      </Container>
+      <ReactQueryDevtools initialIsOpen/>
+    </QueryClientProvider>
   );
 }
 
